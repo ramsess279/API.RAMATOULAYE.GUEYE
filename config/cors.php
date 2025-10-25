0a24b45ce9fb9ae5aa9b4e87750643ae94e1ed45
rama-lpb <ramatoulaye.rtg@gmail.com>
2025-10-25 14:25:45 +0000

    Configuration Initiale du Projet Laravel

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['http://127.0.0.1:8000', 'http://localhost:8000'],
 
     'allowed_origins' => ['*'],
 
