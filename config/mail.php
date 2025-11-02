a01993e580a11975ae6ec4347deb56377f9764c7
rama-lpb <ramatoulaye.rtg@gmail.com>
2025-11-02 18:08:49 +0000

    Revert: Configuration email - retour au SMTP direct pour la production
    - Changement du driver par défaut de 'failover' à 'smtp'
    - Configuration plus simple pour l'environnement de production
    - Tests d'envoi email validés avec succès

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'failover'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
